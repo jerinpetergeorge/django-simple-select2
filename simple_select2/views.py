1dfb3bb31abcc7119fe667549ef59f737f928c14
Jerin Peter George <jerinpetergeorge@gmail.com>
2020-04-01 16:00:05 +0530

    autocomplete base view optimized

--- simple_select2/views.py
+++ simple_select2/views.py
@@ -9,21 +9,19 @@ class AutoCompleteBaseView(BaseListView):
     search_param = 'term'
     search_term = ''
     object_list = None
-    paginate_by = 5
+    paginate_by = 15
     paginator = DjangoPaginator
     filter_class = SearchFilter
     search_fields = ()
+    ordering = ('-id',)
 
-    @staticmethod
-    def get_id_rep(obj):
+    def get_id_rep(self, obj):
         return str(obj.pk)
 
-    @staticmethod
-    def get_text_rep(obj):
+    def get_text_rep(self, obj):
         return str(obj)
 
-    @staticmethod
-    def get_pagination(paginator_page):
+    def get_pagination(self, paginator_page):
         return {'more': paginator_page.has_next()}
 
     def get_results(self, filtered_qs):
@@ -38,9 +36,12 @@ class AutoCompleteBaseView(BaseListView):
     def get_search_fields(self):
         return self.search_fields
 
+    def get_ordering(self):
+        return self.ordering
+
     def get_filtered_queryset(self):
         search_filter = self.filter_class(self.search_param)
-        return search_filter.filter_queryset(self.request, self.get_queryset(), self)
+        return search_filter.filter_queryset(self.request, self.get_queryset(), self).order_by(*self.get_ordering())
 
     def get(self, request, *args, **kwargs):
         self.search_term = request.GET.get(self.search_param, '')
